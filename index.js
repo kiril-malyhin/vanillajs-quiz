a8c00761658c83d5c614bd709910fdead4e46726
CIB\andreib <Andrei.Bogushevich@cib.de>
2017-10-11 14:23:37 +0300

    rendering templates

--- index.js
+++ index.js
@@ -1,25 +1,101 @@
 const data = [
     {
         questionId: 'question1',
-        correctAnswerId: 'var1',
+        questionText: 'How much 30/10?',
+        correctAnswer: '3',
+        options: [
+            '3',
+            '5'
+        ],
+        errorText: 'Incorrect',
+        correctText: 'Correct'
+    },
+    {
+        questionId: 'question2',
+        questionText: 'How much 60/10?',
+        correctAnswer: '6',
+        options: [
+            '3',
+            '6',
+            '7'
+        ],
+        errorText: 'Incorrect',
+        correctText: 'Correct'
     }
 ];
+const quiz = document.querySelector('.quiz');
 
-function checkAnswer(questionId, variantId) {
-    for (let i = 0; i < data.length; i++) {
-        if (data[i].questionId === questionId) {
-            if (data[i].correctAnswerId === variantId) {
-                let question = document.getElementById(questionId);
+data.forEach(question => {
+   quiz.appendChild(renderQuestion(question));
+});
 
-                let correct = question.getElementsByClassName('correct');
-                correct[0].classList.add('showCorrect');
+function renderQuestion(question) {
+    const res = document.createElement('div');
+    res.innerHTML = `
+    <div class="quiz-item">
+        <div class="question">
+            <div class="question-text"></div>
+            <div class="question-options">
+                 
+            </div>
+        </div>
+        <div class="answer">
+            <div class="correct">
+                Correct
+            </div>
+            <div class="incorrect">
+                Incorrect
+            </div>
+        </div>
+    </div>`;
+
+    const quizItem = res.querySelector('.quiz-item');
+    const questionText = res.querySelector('.question-text');
+    const answer = res.querySelector('.answer');
+    const questionOptions = res.querySelector('.question-options');
+    const correct = res.querySelector('.correct');
+    const incorrect = res.querySelector('.incorrect');
+
+    quizItem.id = question.questionId;
+
+    questionText.innerHTML = question.questionText;
+
+    question.options.forEach(option => {
+        questionOptions.appendChild( renderOption(option) );
+    });
+    
+    return res;
+
+
+    function renderOption(option){
+        const resOption = document.createElement('div');
+        resOption.innerHTML = `
+            <label>
+                <span class="option-text"></span>
+                <input type="radio">
+            </label>`;
+        
+        const optionText = resOption.querySelector('.option-text');
+        const checkbox = resOption.querySelector('input');
+
+        optionText.innerHTML = option;
+        checkbox.name = question.questionId;
+
+        checkbox.addEventListener('change', e => {
+            answer.classList.add('showAnswer');
+            correct.classList.remove('showCorrect');
+            incorrect.classList.remove('showIncorrect');
+
+            if (question.correctAnswer === option) {
+                correct.classList.add('showCorrect');
             } else {
-                let question = document.getElementById(questionId);
-                let incorrect = question.getElementsByClassName('incorrect');
-                incorrect[0].classList.add('showIncorrect');
+                incorrect.classList.add('showIncorrect');
             }
-        }
+        })
+
+        return resOption;
     }
 }
+
 
 
